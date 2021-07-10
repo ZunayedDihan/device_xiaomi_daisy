deaded19b3c8758e567f3d0b92a362aaf99a7624
Hotorou <zunayeddihan@gmail.com>
2021-07-10 07:30:27 +0000

    daisy: we are not using common vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -239,4 +239,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/daisy/BoardConfigVendor.mk
--include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
